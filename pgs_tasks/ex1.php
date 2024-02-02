d16de75663bacbff19ee93cf626663fae1f9e4b6
Soorya Rasandi <your.email@example.com>
2024-02-02 13:53:04 +0200

    Ex1 updated

--- pgs_tasks/ex1.php
+++ pgs_tasks/ex1.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>"Exercise 1: Getting Started with PHP-Soorya Rasandi"</title>
-    
-    <?php
+<?php
 $title = "Exercise 1: Getting Started with PHP-Soorya Rasandi";
-include 'header.php' ?> ;
+include 'header.php'; ?> 
 
 <div class="container">
     <div class="row">
